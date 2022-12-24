5b39aec095994e2285919641ef3ef408b5ebba17
0x00646f616e6e63 <54035120+doannc2212@users.noreply.github.com>
2022-12-25 06:07:55 +0700

    refactor

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,8 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+set -gx EDITOR nvim
+set -gx SHELL fish
+set -gx PF_INFO "ascii title os uptime pkgs wm shell editor"
+set -gx BAT_THEME catppuccin
+
 alias vim "nvim"
+
+starship init fish | source
