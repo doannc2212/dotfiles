7e3733360e872bde5eea082fdd07faa739e91d78
0x00646f616e6e63 <doannc2212@gmail.com>
2023-06-03 21:50:55 +0700

    refactor: change from i3wm to bspwm

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,8 @@ set -gx BAT_THEME catppuccin
 # set -x JAVA_HOME '/usr/lib/jvm/java-11-openjdk'
 
 alias vim "nvim"
+alias p "pnpm"
 alias audio "spotify_player"
+alias chat-ops "chat-ops.AppImage"
 
 starship init fish | source
