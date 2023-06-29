1d5f5e9436174eaf9eb76e9e094d3430ac98f78f
0x00646f616e6e63 <doannc2212@gmail.com>
2023-06-29 20:46:34 +0700

    feat: initial 3 monitor config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+BAR_CONFIG=~/.config/polybar/config.ini 
+
+PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
+OTHERS=$(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1)
 
 # Terminate already running bar instances
 killall -q polybar
@@ -6,8 +10,12 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar main --config=~/.config/polybar/config.ini &
-polybar second --config=~/.config/polybar/config.ini &
+
+# Launch on primary monitor
+TRAY=right MONITOR=$PRIMARY polybar --reload -c $BAR_CONFIG main &
+# Launch on all other monitors
+for m in $OTHERS; do
+  TRAY=none MONITOR=$m polybar --reload -c $BAR_CONFIG second &
+done
 
 echo "Polybar launched..."
---
+++ .screenlayout/3screen.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --off --output DisplayPort-1 --mode 1920x1080 --pos 0x355 --rotate normal --output DisplayPort-2 --mode 1920x1080 --pos 3840x0 --rotate right --output HDMI-A-0 --primary --mode 1920x1080 --pos 1920x355 --rotate normal --output DVI-D-0 --off
