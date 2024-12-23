e2944740399c543c55e886554b347a522da42dda
0x00646f616e6e63 <doannc2212@gmail.com>
2024-12-23 18:34:58 +0700

    Config two monitor with polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,6 +7,13 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
+# polybar main --config=~/.config/polybar/config.ini &
+# xrandr --query | grep " connected" | cut -d" " -f1 | wc -l
+# if there's only one monitor, launch polybar main
+# else if there are more than two monitors, launch polybar main and second
 polybar main --config=~/.config/polybar/config.ini &
+if [ $(xrandr --query | grep " connected" | cut -d" " -f1 | wc -l) -gt 1 ]; then
+  polybar second --config=~/.config/polybar/config.ini &
+fi
 
 echo "Polybar launched..."
